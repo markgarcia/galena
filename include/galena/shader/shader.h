1ebab3f3710283ffefbb5df7ccc87943cd050098
Mark Garcia <markwynngarcia@outlook.com>
2016-07-12 20:35:09 +0800

    Removed shader-identifying macros.

--- include/galena/shader/shader.h
+++ include/galena/shader/shader.h
@@ -1,23 +1,5 @@
 #pragma once
 
-#ifdef GALENA_SHADER_COMPILER
-
-#ifndef GALENA_VERTEX_SHADER
-#define GALENA_VERTEX_SHADER [[galena_vertex_shader]]
-#endif
-
-#ifndef GALENA_PIXEL_SHADER
-#define GALENA_PIXEL_SHADER [[galena_pixel_shader]]
-#endif
-
-#else
-
-#define GALENA_VERTEX_SHADER
-#define GALENA_PIXEL_SHADER
-
-#endif
-
-
 namespace galena {
 
     struct float4 {
--- tests/chaos/shader.cpp
+++ tests/chaos/shader.cpp
@@ -1,12 +1,11 @@
 #include <galena/shader/shader.h>
 
 
-GALENA_VERTEX_SHADER galena::float4 vertex_shader(galena::float4 pos) {
+galena::float4 vertex_shader(galena::float4 pos) {
     return pos;
 }
 
 
-GALENA_PIXEL_SHADER galena::float4 pixel_shader(galena::float4)
-{
+galena::float4 pixel_shader(galena::float4) {
     return { 1.0f, 1.0f, 0.0f, 1.0f };
 }
